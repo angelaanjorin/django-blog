afd32abb7d9890bf6688d55f96b19d34824166ee
Angela Anjorin <angela.anjorin@gmail.com>
2024-06-27 11:15:37 +0000

    Add an update on function to the post model and make migrations.

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,10 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a)j7!435r)yb7(@&hbr7*(h9h*2d!udc^1)8a0u*_j1tyduhjs'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['8000-angelaanjori-djangoblog-nhf6ixpdp4v.ws-eu114.gitpod.io',
-'8000-angelaanjori-djangoblog-7zea0ae6dz9.ws-eu114.gitpod.io',
+ALLOWED_HOSTS = ['8000-angelaanjori-djangoblog-10nkl6arf09.ws-eu114.gitpod.io',
                 '.herokuapp.com'
 ]
 
