355350037bb1bade9bdd9ab8ffb728e214f6f19d
Angela Anjorin <angela.anjorin@gmail.com>
2024-06-28 09:17:18 +0000

    Create a comment modal, migrate it to the database and add a class Meta to both the post and comment models to order the inputs.

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-angelaanjori-djangoblog-10nkl6arf09.ws-eu114.gitpod.io',
+ALLOWED_HOSTS = ['8000-angelaanjori-djangoblog-gbzrpka1vlf.ws-eu114.gitpod.io',
                 '.herokuapp.com'
 ]
 
