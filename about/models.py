6a9aabe9b8b86cdacc53d3a19fad3b4e468feadb
Angela Anjorin <angela.anjorin@gmail.com>
2024-07-05 10:50:44 +0000

    Add a collaboration form to the about_me app.

--- about/models.py
+++ about/models.py
@@ -4,11 +4,19 @@ from django.db import models
 
 
 class About(models.Model):
-    title = models.CharField(max_length=200, unique=True)
-    content = models.TextField()
+    title = models.CharField(max_length=200)
     updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
 
-    class Meta:
-        ordering = ["-updated_on"]
     def __str__(self):
         return self.title
+
+
+class CollaborateRequest(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+    message = models.TextField()
+    read = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Collaboration request from {self.name}"
